5a3a94262356fc6765127506bb541a2ba84f0f13
liuxin <819238426@qq.com>
2018-05-16 17:35:47 +0800

    新建前端工程框架

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-unused-vars": [2, {
+      // 允许声明未使用变量
+      "vars": "local",
+      // 参数不检查
+      "args": "none"
+    }],
+    // 关闭语句强制分号结尾
+    "semi": [0],
+    //空行最多不能超过100行
+    "no-multiple-empty-lines": [0, {"max": 100}],
+    //关闭禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": [0],
+    //不以新行开始的块{前面要不要有空格）
+    "space-before-blocks":[0,"always"],
+    "space-before-function-paren":[0,"always"],
+    "space-before-function-paren": [0, "always"],//函数定义时括号前面要不要有空格
+    "space-in-parens": [0, "never"],//小括号里面要不要有空格
+    "space-infix-ops": 0,//中缀操作符周围要不要有空格
+    "space-return-throw-case": 0,//return throw case后面要不要加空格
+    "space-unary-ops": [0, { "words": true, "nonwords": false }],//一元运算符的前/后要不要加空格
+    "spaced-comment": 0,//注释风格要不要有空格什么的
+    //关闭不能有未定义的变量
+    "no-undef":[0],
+    "indent":[0],
+    "key-spacing":[0],
+    "keyword-spacing":[0],
+    "quotes":[0]
+  }
+}
---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1 class="text-danger text-center">{{msg}}</h1>
+</template>
+
+<script>
+  import axios from 'axios'
+    export default {
+        name: "index",
+      data(){
+          return{
+            msg:'dark light'
+          }
+      },
+      mounted(){
+        console.log($('h2').text());
+        axios.get('/static/test.json')
+          .then(res => {
+            console.log(res.data);
+          });
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
