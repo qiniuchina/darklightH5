f8609543a9bec9efaf0f9016d4810b606d035ecc
liuxin <819238426@qq.com>
2018-06-26 15:16:48 +0800

    add img

--- src/components/BlackSwan/StocksAnalysesShow.vue
+++ src/components/BlackSwan/StocksAnalysesShow.vue
@@ -2,14 +2,14 @@
   <div class="container-fluid">
     <h3 class="page-title text-center" style="padding: 20px">股票行情大数据分析</h3>
   <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="/static/img/discussAnly.jpg" alt="舆情分析">
+    <img class="card-img-top" src="/static/img/heart.png" alt="舆情分析">
     <div class="card-body">
       <p class="badge badge-primary">舆情分析</p>
       <p class="card-text">对大盘情绪看涨</p>
     </div>
   </div>
     <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="/static/img/wordsimg.png" alt="股票词云">
+      <img class="card-img-top" src="/static/img/sentiments.png" alt="股票词云">
       <div class="card-body">
         <p class="badge badge-primary">股票词云</p>
         <p class="card-text">大数据词云显示</p>
